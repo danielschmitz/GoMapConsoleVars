5ff6fb2c23ed770b5450e0c8970db39e79c2e7f6
Daniel Schmitz <danieljfa@gmail.com>
2021-04-12 19:25:51 -0300

    Documentacao

--- mapconsolevars.go
+++ mapconsolevars.go
@@ -1,13 +1,17 @@
 /*
-Le os argumentos da inicialização do programa, criando
-um conjunto de chave/valor de acordo com SEPARATOR.
+Lê os argumentos da inicialização do programa (console),
+criando um map chave/valor
 
 Exemplo:
 
-Ao chamar o programa: go run *.go chave1=valor1 chave2=valor2
+Ao chamar o programa: `go run *.go chave1=valor1 chave2=valor2`
 
-Passo 1: No main do programa, chamar ReadArgsConsole()
-Passo 2: Quando precisar do valor de chave1, use GetArgsValue("chave1")
+Quando precisar do valor de chave1, use `GetArgsValue("chave1")`
+
+	valor1, found := mapconsolevars.GetArgsValue("chave1")
+	if found {
+		fmt.Println("O valor de chave1 é", valor1)
+	}
 
 */
 package mapconsolevars
